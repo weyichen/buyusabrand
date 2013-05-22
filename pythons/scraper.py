2ababab2979842ca698c5df0909e6b3620406151
weyichen <weyichen@seas.upenn.edu>
2013-05-22 12:35:51 -0700

    Python scraper

---
+++ pythons/scraper.py
@@ -0,0 +1,21 @@
+from bs4 import BeautifulSoup
+import urllib2
+
+# custom package from Python website (ensure .py file is in same folder)
+from UnicodeWriter import UnicodeWriter
+
+soup = BeautifulSoup(urllib2.urlopen("http://www.21usdeal.com/zh/page/2/"))
+#soup = BeautifulSoup(open("21usdeal_test.html"))
+
+titles = soup.findAll(attrs={'class' : "art-postheader"})
+descrs = soup.findAll(attrs={'class' : "art-postcontent"})
+
+names = [title.a.get_text() for title in titles]
+texts = [descr.get_text() for descr in descrs]
+
+with open('wordpress1.csv', 'wb') as f:
+    writer = UnicodeWriter(f, delimiter=",")
+    writer.writerow(["csv_post_title", "csv_post_post", "csv_post_type", "csv_post_excerpt", "csv_post_categories", "csv_post_tags", "csv_post_date", "desc", "link"])
+    
+    for (name, text) in zip(names, texts):
+        writer.writerow([name, "", "post", name, "csvtest", "", "now", text, ""])
